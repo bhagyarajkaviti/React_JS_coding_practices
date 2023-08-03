01d63383cca6a283b1fe178ee2b8fd58dec99f51
bhagyarajkaviti <bhagyarajkaviti@gmail.com>
2023-08-03 20:02:15 +0530

    React Js CCBP Coding PRctices

---
+++ reactjs/coding-practices/congratsCardApp/index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div>
+    <div>
+      <h1>congratulations</h1>
+    </div>
+    <div class="personCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>kiran V</h1>
+      <p>vishnu Institute of computer education and technology,Bhimavaram.</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+      >
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
